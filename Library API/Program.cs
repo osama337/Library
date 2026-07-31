15af2d13e02040480df58f8ce89d4035182d2cd3
osamatarek <osamatarek380@gmail.com>
2026-07-31 23:42:15 +0300

    .

--- Library API/Program.cs
+++ Library API/Program.cs
@@ -21,12 +21,9 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
